595a11c512b56fe7b3396c3c102f5c772c687733
Rafael Spessotto <rafa_spe@hotmail.com>
2025-06-22 14:00:18 +0100

    Changed driver
    Changed driver to work perfectly!!

--- src/User_Setup.h
+++ src/User_Setup.h
@@ -44,7 +44,7 @@
 //#define RPI_DISPLAY_TYPE // 20MHz maximum SPI
 
 // Only define one driver, the other ones must be commented out
-#define ILI9341_DRIVER       // Generic driver for common displays
+//#define ILI9341_DRIVER       // Generic driver for common displays
 //#define ILI9341_2_DRIVER     // Alternative ILI9341 driver, see https://github.com/Bodmer/TFT_eSPI/issues/1172
 //#define ST7735_DRIVER      // Define additional parameters below for this display
 //#define ILI9163_DRIVER     // Define additional parameters below for this display
@@ -55,7 +55,7 @@
 //#define ILI9486_DRIVER
 //#define ILI9488_DRIVER     // WARNING: Do not connect ILI9488 display SDO to MISO if other devices share the SPI bus (TFT SDO does NOT tristate when CS is high)
 //#define ST7789_DRIVER      // Full configuration option, define additional parameters below for this display
-//#define ST7789_2_DRIVER    // Minimal configuration option, define additional parameters below for this display
+#define ST7789_2_DRIVER    // Minimal configuration option, define additional parameters below for this display
 //#define R61581_DRIVER
 //#define RM68140_DRIVER
 //#define ST7796_DRIVER
@@ -75,7 +75,7 @@
 // For ST7735, ST7789 and ILI9341 ONLY, define the colour order IF the blue and red are swapped on your display
 // Try ONE option at a time to find the correct colour order for your display
 
-//  #define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
+#define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
 //  #define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
 
 // For M5Stack ESP32 module with integrated ILI9341 display ONLY, remove // in line below
@@ -87,11 +87,11 @@
 // #define TFT_WIDTH  128
 // #define TFT_WIDTH  172 // ST7789 172 x 320
 // #define TFT_WIDTH  170 // ST7789 170 x 320
-// #define TFT_WIDTH  240 // ST7789 240 x 240 and 240 x 320
+#define TFT_WIDTH  240 // ST7789 240 x 240 and 240 x 320
 // #define TFT_HEIGHT 160
 // #define TFT_HEIGHT 128
 // #define TFT_HEIGHT 240 // ST7789 240 x 240
-// #define TFT_HEIGHT 320 // ST7789 240 x 320
+#define TFT_HEIGHT 320 // ST7789 240 x 320
 // #define TFT_HEIGHT 240 // GC9A01 240 x 240
 
 // For ST7735 ONLY, define the type of display, originally this was based on the
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 TFT_eSPI tft = TFT_eSPI();
 #define TFT_RST 4 // Make sure this matches your wiring
+#define TFT_SWRESET 0x01 // Software reset command for most TFT controllers
 
 void hardwareReset() {
   pinMode(TFT_RST, OUTPUT);
@@ -12,22 +13,45 @@ void hardwareReset() {
   delay(150);
 }
 
+void drawCreeperFace(int x, int y, int size) {
+  uint16_t green = 0x07E0; // Bright green
+  uint16_t black = TFT_BLACK;
+  float unit = size / 8.0; // Use float for precise placement
+
+  // Fill face background
+  tft.fillRect(x, y, round(unit * 8), round(unit * 8), green);
+
+  // Eyes
+  tft.fillRect(round(x + unit * 1), round(y + unit * 2), round(unit * 2), round(unit * 2), black);
+  tft.fillRect(round(x + unit * 5), round(y + unit * 2), round(unit * 2), round(unit * 2), black);
+
+  // Mouth
+  tft.fillRect(round(x + unit * 2), round(y + unit * 5), round(unit * 4), round(unit * 2), black);
+  tft.fillRect(round(x + unit * 3), round(y + unit * 4), round(unit * 2), round(unit * 3), black);
+}
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Setup running");
-  hardwareReset();
+  //hardwareReset();
   tft.init();
-  tft.setRotation(3);
-  tft.invertDisplay(1); 
-  tft.fillScreen(TFT_BLACK);
-  delay(100);
+  tft.setRotation(0);
+  tft.invertDisplay(1);
+  tft.fillScreen(TFT_BLACK); // Clear after rotation
 }
 
 void loop() {
-  Serial.println("Loop running");
-  tft.fillRect(0, 0, 50, 50, 0x001F);
-  tft.fillRect(50, 0, 50, 50, 0x07E0);
-  tft.fillRect(100, 0, 50, 50, 0xF800);
-  tft.fillRect(150, 0, 50, 50, 0x07FF);
-  delay(1000);
+  static bool drawn = false;
+  if (!drawn) {
+    tft.fillScreen(TFT_BLACK);
+    int faceSize = min(tft.width(), tft.height()) * 3 / 4;
+    int unit = faceSize / 8;
+    int actualFaceSize = unit * 8;
+    int centerOffsetX = 0;
+    int centerOffsetY = 0;
+    int x = (tft.width() - actualFaceSize) / 2 + centerOffsetX;
+    int y = (tft.height() - actualFaceSize) / 2 + centerOffsetY;
+    drawCreeperFace(x, y, actualFaceSize);
+    drawn = true;
+  }
+  delay(100);
 }
