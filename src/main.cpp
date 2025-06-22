57b3b17806e6b8da6657d2c3cf05358f51fb8a6e
Rafael Spessotto <rafa_spe@hotmail.com>
2025-06-22 14:09:15 +0100

    SplashScreen method

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,24 @@ TFT_eSPI tft = TFT_eSPI();
 
 void initDisplay(TFT_eSPI &tft);
 
+void showSplashScreen(){
+  tft.fillScreen(TFT_BLACK);
+  int faceSize = min(tft.width(), tft.height()) * 3 / 4;
+  int unit = faceSize / 8;
+  int actualFaceSize = unit * 8;
+  int centerOffsetX = 0;
+  int centerOffsetY = 0;
+  int x = (tft.width() - actualFaceSize) / 2 + centerOffsetX;
+  int y = (tft.height() - actualFaceSize) / 2 + centerOffsetY;
+  drawCreeperFace(tft, x, y, actualFaceSize);
+}
+
 void setup() {
     Serial.begin(115200);
     initDisplay(tft);
+    showSplashScreen();
 }
 
 void loop() {
-  static bool drawn = false;
-  if (!drawn) {
-    tft.fillScreen(TFT_BLACK);
-    int faceSize = min(tft.width(), tft.height()) * 3 / 4;
-    int unit = faceSize / 8;
-    int actualFaceSize = unit * 8;
-    int centerOffsetX = 0;
-    int centerOffsetY = 0;
-    int x = (tft.width() - actualFaceSize) / 2 + centerOffsetX;
-    int y = (tft.height() - actualFaceSize) / 2 + centerOffsetY;
-    drawCreeperFace(tft, x, y, actualFaceSize);
-    drawn = true;
-  }
-  delay(100);
+  
 }
